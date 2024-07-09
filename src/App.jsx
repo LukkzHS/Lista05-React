c1003c42d5987d44cc92663aafe3e516f5d1782c
LukkzHS <luccahs03@gmail.com>
2024-07-09 12:14:18 -0300

    deploy lista 05 react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import Apresentacao from "./components/Apresentacao"
+import Box from "./components/Box"
+
+
+function App() {
+  return (
+    <>
+    <Apresentacao/>
+    <Box texto="PLANO DE DESENVOLVIMENTO INSTITUCIONAL"/>
+    </>
+)
+}
+export default App
